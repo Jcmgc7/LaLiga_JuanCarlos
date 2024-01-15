5209410a41429a5d255e2946bd42f71eeb125aa3
alvar <alvaro2004gb13@gmail.com>
2024-01-15 14:20:32 +0100

    Login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.la_liga"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.la_liga"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
