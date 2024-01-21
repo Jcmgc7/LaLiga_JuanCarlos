2ea59d587c79ec933898320d4e6753ceb347f4be
alvar <alvaro2004gb13@gmail.com>
2024-01-22 00:11:49 +0100

    hola

--- app/src/main/java/com/example/la_liga/Clasificacion.java
+++ app/src/main/java/com/example/la_liga/Clasificacion.java
@@ -3,18 +3,54 @@ package com.example.la_liga;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class Clasificacion extends AppCompatActivity {
-
+     ArrayAdapter<String> adapter;
+     ListView listView;
+    SQLiteDatabase db;
+    SQLiteHelper helper;
+    Cursor cursor;
     @Override
+    @SuppressLint("Range")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clasificacion2);
+
+        helper = new SQLiteHelper(this);
+        db = helper.getWritableDatabase();
+        int x = 1;
+
+        ArrayList<String> datos = new ArrayList();
+        do {
+            cursor = db.rawQuery("SELECT * FROM Equipos WHERE _id = ?", new String[]{String.valueOf(x)});
+            if (cursor.moveToNext()) {  // Cambiado de moveToFirst a moveToNext
+                String nombreEquipo = cursor.getString(cursor.getColumnIndex("Nombre"));
+                String puntosEquipo = cursor.getString(cursor.getColumnIndex("Puntos"));
+                datos.add(x + "º Nombre:" + nombreEquipo + " | Puntos:" + puntosEquipo);
+            }
+            x = x + 1;
+        } while (cursor.moveToNext());
+
+        // Crear el adaptador
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, datos);
+
+        // Obtener la referencia al ListView y establecer el adaptador
+        listView = findViewById(R.id.lista);
+        listView.setAdapter(adapter);
+
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -48,6 +84,10 @@ public class Clasificacion extends AppCompatActivity {
             return true;
         } else if (itemId == R.id.ModificarEquipos) {
             Intent i = new Intent(getApplicationContext(), Modificar_Equipo.class);
+            startActivity(i);
+            return true;
+        } else if (itemId == R.id.MostrarEquipos) {
+            Intent i = new Intent(getApplicationContext(), Mostrar_Equipos.class);
             startActivity(i);
             return true;
         }
